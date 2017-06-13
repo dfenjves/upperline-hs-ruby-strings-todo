459d9c27a5f5e3c990d0ce545c726669e26273c3
Daniel Fenjves <daniel@upperlinecode.com>
2017-06-13 14:31:37 +0000

    finished my invitation

---
+++ invitation.rb
@@ -0,0 +1,7 @@
+puts "What is the guest name?"
+guest_name = gets.chomp
+puts "What is the party name?"
+party_name = gets.chomp
+
+puts "Dear #{guest_name},"
+puts "Please come to my #{party_name}"
